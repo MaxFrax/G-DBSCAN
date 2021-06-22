65700c921f6af32472a0dfff694219abaee701c2
Massimo <maxfrax@gmail.com>
2021-06-22 15:03:30 +0200

    Adds cluster assignment through bfs

--- gdbscan.cu
+++ gdbscan.cu
@@ -84,6 +84,74 @@ __global__ void compute_adjacency_list(float* dataset, int d, int n, int* degree
 	}
 }
 
+__global__ void kernel_bfs(int * Fa, int * Xa, int n, int * degrees, int * adjListIx, int * adjList) {
+	int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if (tid >= n)
+		return;
+
+	// This could cause warp divergency
+	if(Fa[tid] == 0) {
+		return;
+	}
+
+	Fa[tid] = 0;
+	Xa[tid] = 1;
+
+	int adjListBegin = adjListIx[tid];
+	
+	// Foreach neighbour
+	for(int i = 0; i < degrees[tid]; i++) {
+		int nid = adjList[adjListBegin + i];
+
+		if(Xa[nid] == 0) {
+			Fa[nid] = 1;
+		}
+	}
+}
+
+__global__ void cluster_assignment(int * Xa, int * cluster, int n, int currentCluster) {
+	int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if (tid >= n)
+		return;
+
+	if(Xa[tid] == 1 && cluster[tid] == 0) {
+		cluster[tid] = currentCluster;
+	}
+}
+
+__host__ void bfs(int * Fa, int * Xa, int v, int n, int * cluster, int currentCluster, int * degrees, int * adjListIx, int * adjList){
+	
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+
+	int threadsPerBlock = prop.maxThreadsPerBlock;
+	int blocksPerGrid = (n / threadsPerBlock) + 1;
+
+	bool FaEmpty = false;
+	Fa[v] = 1;
+
+	while(!FaEmpty) {
+
+		kernel_bfs<<<blocksPerGrid, threadsPerBlock>>>(Fa, Xa, n, degrees, adjListIx, adjList);
+		cudaDeviceSynchronize();
+
+		// Checks if the frontier is empty
+		FaEmpty = true;
+		for(int i = 0; i < n; i++) {
+			if(Fa[i] > 0){
+				FaEmpty = false;
+				break;
+			}
+		}
+	}
+
+	// Foreach visited node (Xa == 1) which is not assigned to a cluster, assign it to currentCluster
+	cluster_assignment<<<blocksPerGrid, threadsPerBlock>>>(Xa, cluster, n, currentCluster);
+	cudaDeviceSynchronize();
+}
+
 int main()
 {
 	cudaDeviceProp prop;
@@ -150,11 +218,47 @@ int main()
 		printf("%d\t", adjList[i]);
 	}
 
-	// 7. Free memory
+	// 7. BFS
+
+	int* Xa, *Fa, *cluster;
+	int MinPts = 5;
+	int nextCluster = 1;
+
+	CHECK(cudaMallocManaged(&Xa, n * sizeof(int)));
+	CHECK(cudaMallocManaged(&Fa, n * sizeof(int)));
+	CHECK(cudaMallocManaged(&cluster, n * sizeof(int)));
+
+	CHECK(cudaMemset(Xa, 0, n * sizeof(int)));
+	CHECK(cudaMemset(Fa, 0, n * sizeof(int)));
+
+
+	// Foreach core node:
+	// se core node corrente ha già un cluster = già visitato, skip
+	// altrimenti:
+	// BFS da nodo core corrente
+
+	for (int v = 0; v < n; v++) {
+		if(cluster[v] > 0 || degrees[v] < MinPts)
+			continue;
+
+		bfs(Fa, Xa, v, n, cluster, nextCluster, degrees, adjIndex, adjList);
+		nextCluster++;
+	}
+
+	printf("\nPhase 4: BFS\n");
+
+	for(int i = 0; i < n; i++) {
+		printf("%d\t", cluster[i]);
+	}
+
+	// 8. Free memory
 	CHECK(cudaFree(dataset));
 	CHECK(cudaFree(degrees));
 	CHECK(cudaFree(adjIndex));
 	CHECK(cudaFree(adjList));
+	CHECK(cudaFree(Xa));
+	CHECK(cudaFree(Fa));
+	CHECK(cudaFree(cluster));
 
 	// cudaDeviceReset must be called before exiting in order for profiling and
 	// tracing tools such as Nsight and Visual Profiler to show complete traces.
